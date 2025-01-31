60e828826619892329c8a8b0de1ba7349d2d0074
TabloITAdmin <164124771+TabloITAdmin@users.noreply.github.com>
2025-01-31 17:15:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function hideButton(x)
-{
- x.style.display = 'none';
+function hideButton(x) {
+    x.style.display = 'none';
 }
+
+
+function checkTime() {
+    var currentTime = new Date();
+    var desiredHour = 14; // Set the desired hour (24-hour format) 
+    var desiredMinute = 0; // Set the desired minute 
+    if (currentTime.getHours() >= 10 && currentTime.getHours() <= 14) {
+        document.getElementById("form").style.display = "block";
+    }
+    else {
+        document.getElementById("form").style.display = "none";
+        document.getElementById("alert").style.display = "block";
+    }
+}
+setInterval(checkTime, 1000); // Check the time every second
